5221c479723f4c7dcca670acb33d087b2556d2f1
Rafi R. Ramadhan <root@Rafiramadhan-GPC.localdomain>
2020-12-17 01:45:27 +0700

    adding details page

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import { Button, InteractiveCard, Box, Text, Heading } from "@aksara-ui/core";
+
+import { withRouter } from "react-router-dom";
 
 import "./menu-item.styles.scss";
 
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={`${size} menu-item`}>
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
+  <div
+    className={`${size} menu-item`}
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
+  >
     <div
       className="background-image"
       style={{
@@ -14,7 +20,18 @@ const MenuItem = ({ title, imageUrl, size }) => (
       <h1 className="title">{title.toUpperCase()}</h1>
       <span className="subtitle">SHOP NOW</span>
     </div>
+    {/* <InteractiveCard image={`${imageUrl}`}>
+      
+      <Box p="lg">
+        <Heading scale={400}>{`${title.toUpperCase()}`}</Heading>
+      </Box>
+      <Box p="lg" pt={0}>
+        <Text scale={200} color="grey07">
+          A secure runtime for JavaScript and TypeScript
+        </Text>
+      </Box>
+    </InteractiveCard> */}
   </div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
